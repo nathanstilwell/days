8a9dfa4785b95918c4bd6c3fe95aef0ee7b44ea7
Nathan Stilwell <nathanstilwell@gmail.com>
2020-11-17 21:03:32 -0500

    cutting out console logs

--- script/main.js
+++ script/main.js
@@ -14,6 +14,3 @@ const daysOfMourning = daysSince(deathDay);
 
 doc.getElementById("covid").innerText = toNumerals(daysOfCovid);
 doc.getElementById("death").innerText = toNumerals(daysOfMourning);
-
-console.log("loaded date for ", beginCovid)
-console.log("loaded date for ", deathDay)
