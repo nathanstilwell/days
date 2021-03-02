7ba12c675ee3629773754ef194ff977fa62d7841
Nathan Stilwell <nathanstilwell@cockroachlabs.com>
2021-03-02 13:22:50 -0500

    adding today's date (since I never know what day it is) and rearranging the css classes a little

--- script/main.js
+++ script/main.js
@@ -1,10 +1,12 @@
 import toNumerals from "./modules/numerals.js";
+import formatDate from "./modules/datesSuck.js";
 
 const doc = window.document;
 const humanOffset = 1; // Humans don't start counting at 0;
 const aDay = 24 * 60 * 60 * 1000;
 const beginCovid = new Date("15 MAR 2020");
 const deathDay = new Date("27 APR 2020");
+const today = new Date();
 
 const timeDiff = historicDate => Date.now() - historicDate.getTime();
 const daysSince = when => Math.floor(timeDiff(when) / aDay) + humanOffset;
@@ -12,5 +14,6 @@ const daysSince = when => Math.floor(timeDiff(when) / aDay) + humanOffset;
 const daysOfCovid = daysSince(beginCovid);
 const daysOfMourning = daysSince(deathDay);
 
+doc.getElementById("today").innerText = formatDate(today);
 doc.getElementById("covid").innerText = toNumerals(daysOfCovid);
 doc.getElementById("death").innerText = toNumerals(daysOfMourning);
