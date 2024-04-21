1f2e8b67546176e7cc987fc70c3f4fc35a267fd8
Nathan Stilwell <nathanstilwell@gmail.com>
2024-04-21 17:58:48 -0400

    upgrading the numerals

--- script/modules/numerals.js
+++ script/modules/numerals.js
@@ -1,5 +1,15 @@
-const l = console.log;
-
+const M̅ = 1000000;
+const C̅M̅ = 900000;
+const D̅ = 500000;
+const C̅D̅ = 400000;
+const C̅ = 100000;
+const X̅C̅ = 90000;
+const L̅ = 50000;
+const X̅L̅ = 40000;
+const X̅ = 10000;
+const I̅X̅ = 9000;
+const V̅ = 5000;
+const I̅V̅ = 4000;
 const M = 1000;
 const CM = 900;
 const D = 500;
@@ -15,6 +25,18 @@ const IV = 4;
 const I = 1;
 
 const romanNumerals = [
+  { label: "M̅", value: M̅ },
+  { label: "C̅M̅", value: C̅M̅ },
+  { label: "D̅", value: D̅ },
+  { label: "C̅D̅", value: C̅D̅ },
+  { label: "C̅", value: C̅ },
+  { label: "X̅C̅", value: X̅C̅ },
+  { label: "L̅", value: L̅ },
+  { label: "X̅L̅", value: X̅L̅ },
+  { label: "X̅", value: X̅ },
+  { label: "I̅X̅", value: I̅X̅ },
+  { label: "V̅", value: V̅ },
+  { label: "I̅V̅", value: I̅V̅ },
   { label: "M", value: M },
   { label: "CM", value: CM },
   { label: "D", value: D },
